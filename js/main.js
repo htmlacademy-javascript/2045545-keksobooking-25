cf0b4fafab1578f2e3c9f2b1a9addab177d6144f
Elena <eamatytsina@gmail.com>
2022-02-24 21:41:10 +0300

    getRandomInt and getRandomFloat updated

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0 || min > max) {
+    throw new Error('Min и max не должны быть меньше 0, min должен быть меньше max');
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
-
 }
 
 getRandomInt(5, 17);
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getRandomFloat(min, max, decimals) {
-  return ((Math.random() * (max - min) + min).toFixed(decimals));
-}
 
+const getRandomFloat = (min, max, decimals) => {
+  if (min < 0 || max < 0 || min > max) {
+    throw new Error('Min и max не должны быть меньше 0, min должен быть меньше max');
+  }
+
+  return (Number((Math.random() * (max - min) + min).toFixed(decimals)));
+}
 getRandomFloat(2, 3, 5);
